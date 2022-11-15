5e9250d215950c8462d964515ba0a8357b06bc91
Harsh Prajapati <prajapati.captain@gmail.com>
2022-11-16 01:20:34 +0530

    used arrow

--- Counter/script.js
+++ Counter/script.js
@@ -1,36 +1,47 @@
-
-const decrease = document.getElementById('decrease')
 const number = document.getElementById('count');
-console.log(number);
+
 let counter = 0;
 number.innerText = counter;
 
-const btnDec = function() {
+// const btnDec = function() {
+//     counter--;
+//     number.innerText = counter;
+//     color();
+// }
+// const btnRes = function(){
+//     counter = 0;.
+//     number.innerText = counter;
+// }
+// const btnInc = function (){
+//     counter++;
+//     number.innerText = counter;
+//     color();
+// }
+
+function color() {
+    if (counter == 0) {
+        number.style.color = 'black';
+    } else if (counter > 0) {
+        number.style.color = 'green';
+    } else if (counter < 0) {
+        number.style.color = 'red';
+    }
+}
+
+decrease.addEventListener('click', () => {
     counter--;
     number.innerText = counter;
-    color();
-}
-const btnRes = function(){
+    color()
+});
+reset.addEventListener('click', () => {
     counter = 0;
     number.innerText = counter;
-}
-const btnInc = function (){
+});
+increase.addEventListener('click', () => {
     counter++;
     number.innerText = counter;
     color();
-}
-const color = function (){
-    if(counter == 0){
-        number.style.color = 'black';
-    } else if (counter > 0) {
-      number.style.color = 'green';
-    } else if (counter < 0) {
-      number.style.color = 'red';
-    }
-    }
-decrease.addEventListener('click', btnDec)
-reset.addEventListener('click', btnRes)
-increase.addEventListener('click', btnInc)
+});
 
 
 

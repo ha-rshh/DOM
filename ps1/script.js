6af0969d273b38b7b990e2ae458c596dae590be0
Harsh Prajapati <prajapati.captain@gmail.com>
2022-11-15 18:57:51 +0530

    DOM Practice

---
+++ ps1/script.js
@@ -0,0 +1,26 @@
+// .getElementById
+// const title = document.getElementById('main-heading');
+// console.log(title);
+
+// .getElementByClassName
+// const listItems = document.getElementsByClassName('list-items')
+// console.log(listItems);
+
+//.querySelector
+// const heading = document.querySelector('h1');
+// // console.log(heading)
+
+// // .querySelectorAll
+
+// const listItems = document.querySelectorAll('.list-items'); 
+// console.log(listItems);
+// for(let i = 0 ; i<listItems.length; i++){
+//     listItems[i].style.fontSize = "1rem"
+//     listItems[i].style.color = "black" 
+// }
+// const container = document.querySelector('div');
+// container.style.backgroundColor ="red";
+const color = document.getElementById('hey');
+const bro = document.getElementsByClassName('color');
+const query = document.querySelector("h1")
+query.style.backgroundColor = "Blue"
